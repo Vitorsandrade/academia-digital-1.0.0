6504f2f4a86a473ce718e1793d3aef15ac5a8cfc
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-04-12 01:37:40 -0300

    First Commit

---
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,35 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll(){
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Aluno create(@Valid @RequestBody AlunoForm form){
+        return service.create(form);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id){
+        return service.getAllAvaliacaoFisicaid(id);
+    }
+
+}
---
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -0,0 +1,18 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.dio.academia.digital.entity.Aluno;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvaliacaoFisicaForm {
+
+  private Long alunoId;
+
+  private double peso;
+
+  private double altura;
+}
